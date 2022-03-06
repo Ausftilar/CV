396e747c0ede55b4e688ca59f03e0e25fb1e1d77
Mikhail <mikhailkhudoshin@gmail.com>
2022-03-06 06:32:11 +0400

    feat: add animated first screen

---
+++ dist/js/script.js
@@ -0,0 +1,32 @@
+window.addEventListener('DOMContentLoaded', () => {
+    //Пока без делегирования событий
+    //Буду оптимизировать js код после того, как доделаю все остальное
+    //Не смотрите на него, мне стыдно немного))))
+    const hamburger = document.querySelector('.hamburger'),
+          overlay = document.querySelector('.menu__overlay'),
+          hamburgerEl = document.querySelector('.hamburger__wrapper'),
+          menu = document.querySelector('.menu'),
+          closeElem = document.querySelector('.menu__close');
+    
+    hamburger.addEventListener('click', () => {
+        menu.classList.add('active');
+        hamburgerEl.classList.add('hamburger__wrapper_active');
+    });
+
+    closeElem.addEventListener('click', () => {
+        menu.classList.remove('active');
+        hamburgerEl.classList.remove('hamburger__wrapper_active');
+    });
+
+    overlay.addEventListener('click', () => {
+        menu.classList.remove('active');
+        hamburgerEl.classList.remove('hamburger__wrapper_active');
+    });
+
+    window.addEventListener('keydown', (e) => {
+        if (e.key == 'Escape') {
+            menu.classList.remove('active');
+            hamburgerEl.classList.remove('hamburger__wrapper_active');
+        }
+    });
+});
